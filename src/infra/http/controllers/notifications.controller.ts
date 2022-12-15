d3715aab912a223a714c9be5d5e030f516f10a2f
Jean Martins <jeanmrtns4@gmail.com>
2022-12-15 10:34:44 -0300

    infra and tests

---
+++ src/infra/http/controllers/notifications.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { SendNotification } from '../../../app/use-cases/send-notification';
+import { CreateNotificationDTO } from '../dtos/CreateNotificationDTO';
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
+  @Post()
+  async create(@Body() body: CreateNotificationDTO) {
+    const { notification } = await this.sendNotification.execute({
+      recipientId: body.recipientId,
+      content: body.content,
+      category: body.category,
+    });
+
+    return { notification };
+  }
+}
